a721786a377960a7c9a73fdb13f8462f347f871e
remadex <s.cardon@outlook.com>
2022-01-24 17:18:44 +0100

    update textarea + embroider

--- packages/tpk-ember-textarea/tailwind.config.js
+++ packages/tpk-ember-textarea/tailwind.config.js
@@ -1,8 +1,7 @@
 /* eslint-disable */
 module.exports = {
-    mode: 'jit',
-    purge:  ['./app/**/*.{hbs,js,ts,html}', './tests/**/*.{hbs,js,ts,html}','./addon/**/*.{hbs,js,ts,html}'],
-    corePlugins: {},
-    plugins: [],
-  };
+  content:  ['./app/**/*.{hbs,js,ts,html}', './tests/**/*.{hbs,js,ts,html}','./addon/**/*.{hbs,js,ts,html}'],
+  corePlugins: {},
+  plugins: [],
+};
   
