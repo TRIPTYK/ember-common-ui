7d990ef602abfcda067180c25a00dc2ac9404d66
AmauryD <deflorenne.amaury@gmail.com>
2022-03-16 17:07:06 +0100

    default style

--- packages/tpk-ember-modal/index.js
+++ packages/tpk-ember-modal/index.js
@@ -2,4 +2,12 @@
 
 module.exports = {
   name: require('./package').name,
+  included() {
+    let app = this._findHost();
+    if (app.__emberPowerSelectIncludedInvoked) {
+      this._super.included.apply(this, arguments);
+      return;
+    }
+    app.__emberPowerSelectIncludedInvoked = true;
+  },
 };
