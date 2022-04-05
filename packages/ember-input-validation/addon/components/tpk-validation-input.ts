c06379187ba89cf2ae1bfa374b70b3006a0615f6
AmauryD <deflorenne.amaury@gmail.com>
2022-04-05 16:53:25 +0200

    same update value for both inputs

--- packages/ember-input-validation/addon/components/tpk-validation-input.ts
+++ packages/ember-input-validation/addon/components/tpk-validation-input.ts
@@ -4,8 +4,7 @@ import { BaseValidationArgs, BaseValidationComponent } from './base';
 interface TpkValidationInputArgs extends BaseValidationArgs {}
 
 export default class TpkValidationInput extends BaseValidationComponent<TpkValidationInputArgs> {
-  @action onChange(e: Event) {
-    const value = (e.target as HTMLInputElement).value;
+  @action onChange(value: string) {
     if (this.args.onChange) {
       return this.args.onChange(value);
     }
