c42d9a6e81dffd7b669296ba392479f615f64319
AmauryD <deflorenne.amaury@gmail.com>
2022-07-26 16:08:32 +0200

    test escape close

--- packages/ember-modal/tests/integration/components/modal-test.ts
+++ packages/ember-modal/tests/integration/components/modal-test.ts
@@ -41,44 +41,12 @@ module('Integration | Component | modal', function (hooks) {
 
     await triggerKeyEvent(this.element, 'keyup', 'Escape');
     assert.dom('[data-test-modal-toggle]').doesNotExist();
-    assert.false(this.get('isOpen'));
-  });
-
-  test('click outside', async function (assert) {
-    this.set('onClose', () => {
-      this.set('isOpen', false);
-    });
-    this.set('isOpen', false);
-    this.set('title', 'My modal');
-
-    await render(hbs`
-    <div id="tpk-modal"></div>
-
-    <TpkModal
-      @isOpen={{this.isOpen}}
-      @title={{this.title}}
-      @onClose={{this.onClose}}
-      data-test-modal-toggle
-    as |M|>
-      <M.Content>
-      Content
-      <button type="button" data-test-other class="absolute top-0 z-30 p-5">Banana</button>
-      </M.Content>
-    </TpkModal>`);
-
-    assert.dom('[data-test-modal-toggle]').doesNotExist();
+    assert.false(this.get('isOpen'), 'Esc closes modal');
 
     this.set('isOpen', true);
 
-    assert.dom('[data-test-modal-toggle]').exists('modal should be open');
-    /**
-     * The modifier seems to take some time to listen (some ms), we must wait a little
-     */
-    await new Promise((res) => setTimeout(res, 50));
-    await click('[data-test-other]');
-    assert.true(
-      this.get('isOpen'),
-      'Modal should stay opened even with absolute element'
-    );
+    await click(this.element);
+    assert.dom('[data-test-modal-toggle]').doesNotExist();
+    assert.false(this.get('isOpen'), 'Click outside closes modal');
   });
 });
