3d0caa6fe4eea9fb22072a75d6b6873e14370e0d
AmauryD <deflorenne.amaury@gmail.com>
2022-07-30 00:00:38 +0200

    handling multi

--- packages/ember-select/tests/dummy/app/components/ui-show.ts
+++ packages/ember-select/tests/dummy/app/components/ui-show.ts
@@ -1,17 +1,30 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { TrackedArray } from 'tracked-built-ins';
 
 interface UiShowArgs {}
 
 export default class UiShow extends Component<UiShowArgs> {
   @tracked value = '';
+  @tracked selections: string[] = new TrackedArray([]);
 
   options = ['a', 'b', 'c'];
   @action
   setValue(value: any) {
-    console.log(value);
     this.value = value;
+  }
+
+  @action
+  setValueMultiple(value: any, alreadyInSelection: boolean) {
+    console.log(value, alreadyInSelection);
+    if (!alreadyInSelection) {
+      this.selections.push(value);
+    } else {
+      this.selections = this.selections.filter((f) => f !== value);
+    }
+
+    console.log(this.selections);
   }
 
   @action
