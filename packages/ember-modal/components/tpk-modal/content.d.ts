540dbad51785833f420f6b973d4f670662606545
AmauryD <deflorenne.amaury@gmail.com>
2022-07-29 21:11:59 +0200

    rm dist files

--- packages/ember-modal/components/tpk-modal/content.d.ts
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-interface UiModalContentArgs {
-    onClose: () => void;
-    onClickOutside?: (_e: PointerEvent) => void;
-}
-export default class UiModalContent extends Component<UiModalContentArgs> {
-    onClickOutside(e: PointerEvent): void;
-}
-export {};
--- packages/ember-modal/modifiers/focus-trap.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Options } from 'focus-trap';
-declare const _default: import("ember-modifier").FunctionBasedModifier<{
-    Args: {
-        Named: Options;
-        Positional: unknown[];
-    };
-    Element: HTMLElement;
-}>;
-export default _default;
