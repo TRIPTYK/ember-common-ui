bc411ebeac39a6e194e56b1593950f670d7ed666
AmauryD <deflorenne.amaury@gmail.com>
2023-02-24 17:27:27 +0100

    remove block by default

--- packages/ember-input/tests/integration/components/tpk-input-textarea-test.ts
+++ packages/ember-input/tests/integration/components/tpk-input-textarea-test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable qunit/require-expect */
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, fillIn, findAll } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { getOwner } from '@ember/application';
 import ApplicationInstance from '@ember/application/instance';
@@ -10,43 +10,6 @@ import { a11yAudit } from 'ember-a11y-testing/test-support';
 
 module('Integration | Component | tpk-area', function (hooks) {
   setupRenderingTest(hooks);
-
-  test('class/less by default', async function (assert) {
-    this.set('classless', false);
-    await render(
-      hbs`<TpkTextarea @classless={{this.classless}} @label="label" @value="value"/>`
-    );
-
-    assert.dom('.tpk-textarea-label').exists().containsText('label');
-    assert.dom('.tpk-textarea-input').exists().hasValue('value');
-    assert.dom('.tpk-textarea').exists();
-
-    this.set('classless', true);
-
-    findAll('*').forEach((e) => {
-      assert.dom(e).hasNoClass(/tpk-.*/);
-    });
-  });
-
-  test('input by default', async function (assert) {
-    this.set('change', function (e: string) {
-      assert.step('step');
-      assert.strictEqual(e, 'data');
-    });
-
-    await render(
-      hbs`<TpkTextarea @onChange={{this.change}} @label="label" @value="value"/>`
-    );
-
-    await fillIn('[data-test-tpk-textarea-input]', 'data');
-    assert
-      .dom('[data-test-tpk-textarea-input]')
-      .hasAttribute('id', { any: true });
-    assert
-      .dom('[data-test-tpk-textarea-label]')
-      .hasAttribute('for', { any: true });
-    assert.verifySteps(['step']);
-  });
 
   test('input yield only', async function (assert) {
     await render(
