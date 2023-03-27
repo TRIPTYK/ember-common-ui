8c1213d1ca9b9b7e06c7b15772ac70d652968eb5
DramixDW <verliefden.romain@gmail.com>
2023-03-27 16:01:37 +0200

    Add confirm modal to ember-common-ui

---
+++ packages/ember-modal/modifiers/focus-trap.d.ts
@@ -0,0 +1,11 @@
+import type { Options } from 'focus-trap';
+declare const _default: import("ember-modifier").FunctionBasedModifier<{
+    Args: {
+        Named: {
+            options: Options;
+        };
+        Positional: unknown[];
+    };
+    Element: HTMLElement;
+}>;
+export default _default;
