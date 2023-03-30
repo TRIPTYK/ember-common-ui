5b3a8975c3a755158b7535b0a1ce12cc6346b44c
AmauryD <deflorenne.amaury@gmail.com>
2023-03-30 16:59:41 +0200

    fix strict

--- packages/ember-input/tests/integration/components/tpk-radio-test.ts
+++ packages/ember-input/tests/integration/components/tpk-radio-test.ts
@@ -91,7 +91,7 @@ module('Integration | Component | ui/radio', function (hooks) {
 
   test('Accessibility', async function (assert) {
     await render(
-      hbs`<TpkRadio @onChange={{this.change}} @label="label" @selected="luc" @value="jean" @name="cule" as |O| />`
+      hbs`<TpkRadio @onChange={{this.change}} @label="label" @selected="luc" @value="jean" @name="cule" />`
     );
 
     await a11yAudit();
