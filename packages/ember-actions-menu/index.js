9a04610088c8d60d245f85aa6c78f5066ecd0d01
AmauryD <deflorenne.amaury@gmail.com>
2023-05-04 14:58:49 +0200

    try multi lock file

--- packages/ember-actions-menu/index.js
+++ packages/ember-actions-menu/index.js
@@ -2,12 +2,4 @@
 
 module.exports = {
   name: require('./package').name,
-  included() {
-    let app = this._findHost();
-    if (app.__emberPowerSelectIncludedInvoked) {
-      this._super.included.apply(this, arguments);
-      return;
-    }
-    app.__emberPowerSelectIncludedInvoked = true;
-  },
 };
