2c5a7c30694377827e7e438846241d756de17987
AmauryD <deflorenne.amaury@gmail.com>
2023-07-03 10:44:52 +0200

    select set by default

--- packages/ember-input-validation/addon/components/tpk-validation-select.ts
+++ packages/ember-input-validation/addon/components/tpk-validation-select.ts
@@ -1,5 +1,13 @@
+import { action } from '@ember/object';
 import { BaseValidationArgs, BaseValidationComponent } from './base';
 
 interface TpkValidationSelectArgs extends BaseValidationArgs {}
 
-export default class TpkValidationSelect extends BaseValidationComponent<TpkValidationSelectArgs> {}
+export default class TpkValidationSelect extends BaseValidationComponent<TpkValidationSelectArgs> {
+  @action onChange(value: string) {
+    if (this.args.onChange) {
+      return this.args.onChange(value);
+    }
+    this.args.changeset.set(this.args.validationField, value);
+  }
+}
