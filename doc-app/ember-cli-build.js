8c2133c314e63dbec03e7ac81c6e76d97f5b8f49
AmauryD <deflorenne.amaury@gmail.com>
2023-09-28 23:46:25 +0200

    use doc app

---
+++ doc-app/ember-cli-build.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+
+module.exports = function (defaults) {
+  process.on('uncaughtException', (err) => {
+    // eslint-disable-next-line no-console
+    console.error(err.stack);
+  });
+  let app = new EmberAddon(defaults, {});
+
+  /*
+    This build file specifies the options for the dummy test app of this
+    addon, located in `/tests/dummy`
+    This build file does *not* influence how the addon or the app using it
+    behave. You most likely want to be modifying `./index.js` or app's build file
+  */
+
+  // const { maybeEmbroider } = require('@embroider/test-setup');
+  // return maybeEmbroider(app, {
+  //   skipBabel: [
+  //     {
+  //       package: 'qunit',
+  //     },
+  //   ],
+  // });
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticComponents: true,
+  });
+};
---
+++ doc-app/tests/dummy/app/routes/application.js
@@ -0,0 +1,37 @@
+import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+import { rest } from 'msw';
+
+export default class ApplicationRoute extends Route {
+  @service intl;
+
+  async beforeModel() {
+    this.intl.setLocale(['en-us']);
+    // @ts-ignore
+    const { worker } = await import('/worker.js');
+    await worker([
+      rest.get('/users', (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.json({
+            meta: {
+              total: 1,
+              page: 1,
+            },
+            data: [
+              {
+                type: 'user',
+                id: '1',
+                attributes: {
+                  firstName: 'John',
+                  lastName: 'Doe',
+                  email: 'john.doe@example.com',
+                },
+              },
+            ],
+          }),
+        );
+      }),
+    ]).start();
+  }
+}
