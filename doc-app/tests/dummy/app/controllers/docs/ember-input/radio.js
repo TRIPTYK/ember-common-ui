6829838f073847991e5a482bfe87f191f8dca123
Gilles Bertrand <gilles@triptyk.eu>
2023-11-15 16:16:13 +0100

    add radio button documentation

---
+++ doc-app/tests/dummy/app/controllers/docs/ember-input/radio.js
@@ -0,0 +1,12 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from 'tracked-built-ins';
+
+export default class DocsTpkInputController extends Controller {
+  @tracked value = '';
+
+  @action
+  setRadio(value) {
+    this.value = value;
+  }
+}
