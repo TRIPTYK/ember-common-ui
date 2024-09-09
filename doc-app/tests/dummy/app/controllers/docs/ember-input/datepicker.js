548a56969e805b0d011295ed720138c8b794bdae
remadex <s.cardon@outlook.com>
2024-09-09 18:03:29 +0200

    no ts in js file...

--- doc-app/tests/dummy/app/controllers/docs/ember-input/datepicker.js
+++ doc-app/tests/dummy/app/controllers/docs/ember-input/datepicker.js
@@ -5,9 +5,9 @@ import { tracked } from 'tracked-built-ins';
 export default class DocsTpkDatepickerController extends Controller {
   @tracked selectedDate = undefined;
   @tracked selectedDates = undefined;
-  @tracked date: Date = new Date(2022, 10, 15);
-  @tracked minDate: Date = new Date(2022, 10, 13);
-  @tracked maxDate: Date = new Date(2022, 10, 16);
+  @tracked date = new Date(2022, 10, 15);
+  @tracked minDate = new Date(2022, 10, 13);
+  @tracked maxDate = new Date(2022, 10, 16);
 
   @action
   onChange(dates) {
